1675a0938fd86b9d6e19fa57ac6666b658c046fe
RickyRaMx_c149 <ricardo.rmeza@live.com>
2022-10-05 20:01:54 -0500

    💄 menu responsive

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Image from "next/future/image"
 import Link from "next/link"
 import { useRouter } from "next/router"
@@ -7,7 +8,7 @@ import styles from '../styles/Header.module.css'
 export default function Header() {
   const router = useRouter()
 
-
+  const [open, setOpen] = useState(false);
   return (
     <header className={styles.header}>
         {/* <div className={`contenedor ${styles.barra}`}> */}
@@ -25,27 +26,75 @@ export default function Header() {
               </Link>
           </div>
             <nav className={styles.navegacion}>
-              <Link href='/'>
+            <li className={styles.listItem}>
+            <Link href='/'>
                 <a className={router.pathname === '/' ? styles.active : ''}>
                   Inicio
                 </a> 
                 </Link>
+            </li>
+            <li className={styles.listItem}>
               <Link href='/nosotros'>
                 <a className={router.pathname === '/nosotros' ? styles.active : ''}>
                   Nosotros
                 </a>
               </Link>
-              
+            </li>
+            <li className={styles.listItem}>
               <Link href='/emprendedores'>
                 <a className={router.pathname === '/emprendedores' ? styles.active : ''}>
                   Emprendedores
                 </a>
               </Link>
+            </li>
+            <li className={styles.listItem}>
               <Link href='/contacto'>
                 <a className={router.pathname === '/contacto' ? styles.active : ''}>
                   Contacto
                 </a>
               </Link>
+            </li>
+              
+              {/* MENU RESPONSIE */}
+              <div className={styles.hamburger} onClick={() => setOpen(!open)}>
+                <div className={styles.line} />
+                <div className={styles.line} />
+                <div className={styles.line} />
+              </div>
+              <ul onClick={()=>setOpen(false)} 
+                className={styles.menu} 
+                style={{ right: open ? "0px" : "-50vw" }}
+              >
+                <li className={styles.menuItem}>
+                  <Link href="/">
+                    <a className={router.pathname === '/' ? styles.active : ''}>
+                      Inicio
+                    </a> 
+                  </Link>
+                </li>
+                <li className={styles.menuItem}>
+                  <Link href='/nosotros'>
+                    <a className={router.pathname === '/nosotros' ? styles.active : ''}>
+                      Nosotros
+                    </a>
+                  </Link>
+                </li>
+                <li className={styles.menuItem}>
+                  <Link href='/emprendedores'>
+                    <a className={router.pathname === '/emprendedores' ? styles.active : ''}>
+                      Emprendedores
+                    </a>
+                  </Link>
+                </li>
+                <li className={styles.menuItem}>
+                  <Link href='/contacto'>
+                    <a className={router.pathname === '/contacto' ? styles.active : ''}>
+                      Contacto
+                    </a>
+                  </Link>
+                </li>
+
+              </ul>
             </nav>
         </div>
     </header>
