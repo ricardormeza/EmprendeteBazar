4de8594fd845b5794179355c794fdad3e3d60592
RickyRaMx_c149 <ricardo.rmeza@live.com>
2022-10-14 13:40:26 -0500

    💄 emprendedor section

---
+++ components/emprendedores/EmprendedorUno.js
@@ -0,0 +1,212 @@
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/future/image'
+import styles from '../../styles/Emprendedores.module.css'
+import { FaFacebookSquare, FaInstagram } from 'react-icons/fa'
+import {BsFacebook, BsInstagram} from 'react-icons/bs'
+
+export default function EmprendedorUno() {
+  return (
+    <section className='contenedor'>
+            <h3 className={styles.fraseEmprendedor}><strong>La manera de empezar es dejar de hablar y empezar a hacer.</strong></h3>
+            <div className={styles.emprendedorGrid}>
+                <div className={styles.emprededorItem}>
+                    <Image 
+                        src='/emprendedor/logo_queenCafe.jpg'
+                        alt='Logo de Queen Cafe'
+                        width={200}
+                        height={200}
+                        layout="responsive"
+                    />
+                    <div className={styles.emprendedorItemContent}>
+                        <h2>Queen Cafe</h2>
+                        <div className={styles.ContentItems}>
+                            <Link href='https://www.facebook.com/QueenCafeOficial'>
+                                <a target='_blank' title='Ir fanpage Meta Queen Cafe'>
+                                <BsFacebook className={styles.emprendedoresRedes}/>
+                                </a>
+                            </Link>
+                            <Link href='https://www.instagram.com/queencafeoficial/'>
+                                <a target='_blank' title='Ir a Instagram Queen Cafe'>
+                                <BsInstagram className={styles.emprendedoresRedes}/>
+                                </a>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.emprededorItem}>
+                    <Image 
+                        src='/emprendedor/logo_queenCafe.jpg'
+                        alt='Logo de Queen Cafe'
+                        width={200}
+                        height={200}
+                        layout="responsive"
+                    />
+                    <div className={styles.emprendedorItemContent}>
+                        <h2>Queen Cafe</h2>
+                        <div className={styles.ContentItems}>
+                        <Link href='https://www.facebook.com/QueenCafeOficial'>
+                                <a target='_blank' title='Ir fanpage Meta Queen Cafe'>
+                                <BsFacebook className={styles.emprendedoresRedes}/>
+                                </a>
+                            </Link>
+                            <Link href='https://www.instagram.com/queencafeoficial/'>
+                                <a target='_blank' title='Ir a Instagram Queen Cafe'>
+                                <BsInstagram className={styles.emprendedoresRedes}/>
+                                </a>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.emprededorItem}>
+                    <Image 
+                        src='/emprendedor/logo_queenCafe.jpg'
+                        alt='Logo de Queen Cafe'
+                        width={200}
+                        height={200}
+                        layout="responsive"
+                    />
+                    <div className={styles.emprendedorItemContent}>
+                        <h2>Queen Cafe</h2>
+                        <div className={styles.ContentItems}>
+                        <Link href='https://www.facebook.com/QueenCafeOficial'>
+                                <a target='_blank' title='Ir fanpage Meta Queen Cafe'>
+                                <BsFacebook className={styles.emprendedoresRedes}/>
+                                </a>
+                            </Link>
+                            <Link href='https://www.instagram.com/queencafeoficial/'>
+                                <a target='_blank' title='Ir a Instagram Queen Cafe'>
+                                <BsInstagram className={styles.emprendedoresRedes}/>
+                                </a>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.emprededorItem}>
+                    <Image 
+                        src='/emprendedor/logo_queenCafe.jpg'
+                        alt='Logo de Queen Cafe'
+                        width={200}
+                        height={200}
+                        layout="responsive"
+                    />
+                    <div className={styles.emprendedorItemContent}>
+                        <h2>Queen Cafe</h2>
+                        <div className={styles.ContentItems}>
+                        <Link href='https://www.facebook.com/QueenCafeOficial'>
+                                <a target='_blank' title='Ir fanpage Meta Queen Cafe'>
+                                <BsFacebook className={styles.emprendedoresRedes}/>
+                                </a>
+                            </Link>
+                            <Link href='https://www.instagram.com/queencafeoficial/'>
+                                <a target='_blank' title='Ir a Instagram Queen Cafe'>
+                                <BsInstagram className={styles.emprendedoresRedes}/>
+                                </a>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+                {/* EMPRENDEDOR */}
+                <div className={styles.emprededorItem}>
+                    <Image 
+                        src='/emprendedor/logo_queenCafe.jpg'
+                        alt='Logo de Queen Cafe'
+                        width={200}
+                        height={200}
+                        layout="responsive"
+                    />
+                    <div className={styles.emprendedorItemContent}>
+                        <h2>Queen Cafe</h2>
+                        <div className={styles.ContentItems}>
+                            <Link href='https://www.facebook.com/QueenCafeOficial'>
+                                <a target='_blank' title='Ir fanpage Meta Queen Cafe'>
+                                <BsFacebook className={styles.emprendedoresRedes}/>
+                                </a>
+                            </Link>
+                            <Link href='https://www.instagram.com/queencafeoficial/'>
+                                <a target='_blank' title='Ir a Instagram Queen Cafe'>
+                                <BsInstagram className={styles.emprendedoresRedes}/>
+                                </a>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+                {/* EMPRENDEDOR */}
+                <div className={styles.emprededorItem}>
+                    <Image 
+                        src='/emprendedor/logo_queenCafe.jpg'
+                        alt='Logo de Queen Cafe'
+                        width={200}
+                        height={200}
+                        layout="responsive"
+                    />
+                    <div className={styles.emprendedorItemContent}>
+                        <h2>Queen Cafe</h2>
+                        <div className={styles.ContentItems}>
+                            <Link href='https://www.facebook.com/QueenCafeOficial'>
+                                <a target='_blank' title='Ir fanpage Meta Queen Cafe'>
+                                <BsFacebook className={styles.emprendedoresRedes}/>
+                                </a>
+                            </Link>
+                            <Link href='https://www.instagram.com/queencafeoficial/'>
+                                <a target='_blank' title='Ir a Instagram Queen Cafe'>
+                                <BsInstagram className={styles.emprendedoresRedes}/>
+                                </a>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+                {/* EMPRENDEDOR */}
+                <div className={styles.emprededorItem}>
+                    <Image 
+                        src='/emprendedor/logo_queenCafe.jpg'
+                        alt='Logo de Queen Cafe'
+                        width={200}
+                        height={200}
+                        layout="responsive"
+                    />
+                    <div className={styles.emprendedorItemContent}>
+                        <h2>Queen Cafe</h2>
+                        <div className={styles.ContentItems}>
+                            <Link href='https://www.facebook.com/QueenCafeOficial'>
+                                <a target='_blank' title='Ir fanpage Meta Queen Cafe'>
+                                <BsFacebook className={styles.emprendedoresRedes}/>
+                                </a>
+                            </Link>
+                            <Link href='https://www.instagram.com/queencafeoficial/'>
+                                <a target='_blank' title='Ir a Instagram Queen Cafe'>
+                                <BsInstagram className={styles.emprendedoresRedes}/>
+                                </a>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+                {/* EMPRENDEDOR */}
+                <div className={styles.emprededorItem}>
+                    <Image 
+                        src='/emprendedor/logo_queenCafe.jpg'
+                        alt='Logo de Queen Cafe'
+                        width={200}
+                        height={200}
+                        layout="responsive"
+                    />
+                    <div className={styles.emprendedorItemContent}>
+                        <h2>Queen Cafe</h2>
+                        <div className={styles.ContentItems}>
+                            <Link href='https://www.facebook.com/QueenCafeOficial'>
+                                <a target='_blank' title='Ir fanpage Meta Queen Cafe'>
+                                <BsFacebook className={styles.emprendedoresRedes}/>
+                                </a>
+                            </Link>
+                            <Link href='https://www.instagram.com/queencafeoficial/'>
+                                <a target='_blank' title='Ir a Instagram Queen Cafe'>
+                                <BsInstagram className={styles.emprendedoresRedes}/>
+                                </a>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </section>
+  )
+}
--- pages/emprendedores.js
+++ pages/emprendedores.js
@@ -1,4 +1,7 @@
 import Layout from "../components/Layout"
+import styles from '../styles/Emprendedores.module.css'
+import EmprendedorUno from "../components/emprendedores/EmprendedorUno"
+import SeccionRedes from '../components/redes/SeccionRedes'
 
 export default function Emprendedores() {
   return (
@@ -8,8 +11,13 @@ export default function Emprendedores() {
         description={'Conoce a nuestros emprendedores que participan en Emprendete Bazar el sitio lider de compra y venta de productos locales en Tuxtla Gutiérrez, Chiapas'}
         >
           <div className="contenedor">
-            <h1>Emprendete Bazar</h1>
-            <h2>Nuestros Emprendedores</h2>
+            <div className={styles.emprendedoresEncabezado}>
+              <h1>Emprendete Bazar</h1>
+              <h2>Nuestros <span>Emprendedores</span></h2>
+            </div>
+            <EmprendedorUno/>
+            <br/>
+            <SeccionRedes/>
           </div>
         </Layout>
     </>
